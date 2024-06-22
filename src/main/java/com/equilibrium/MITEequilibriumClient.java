fa956de171ea60785053cfff8c4807a9548c4b99
Lithewings <2631810335@qq.com>
2024-06-23 02:59:10 +0800

    Changes

---
+++ src/main/java/com/equilibrium/MITEequilibriumClient.java
@@ -0,0 +1,10 @@
+package com.equilibrium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MITEequilibriumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
