6b38a057beb02e66fb088105125b52a51b89900a
Lithewings <128245561+Lithewings@users.noreply.github.com>
2024-07-20 16:17:01 +0800

    Delete src/main/java/com/equilibrium directory
    t

--- src/main/java/com/equilibrium/test.java
+++
@@ -1,9 +0,0 @@
-package com.equilibrium;
-
-import net.minecraft.world.dimension.NetherPortal;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(1);
-    }
-}
