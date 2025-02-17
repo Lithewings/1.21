4dfd4dbf4e96ca8843b842caaaccb3e11cc620f0
Lithewings <2631810335@qq.com>
2025-02-17 23:22:17 +0800

    那个白

--- src/main/java/com/equilibrium/mixin/vanilla_blocksmixin/BlocksMixin.java
+++ src/main/java/com/equilibrium/mixin/vanilla_blocksmixin/BlocksMixin.java
@@ -2,8 +2,11 @@ package com.equilibrium.mixin.vanilla_blocksmixin;
 
 import net.minecraft.block.*;
 import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,8 +29,24 @@ public class BlocksMixin {
 //    public static Block register(String id, Block block) {
 //        return Registry.register(Registries.BLOCK, id, block);
 //    }
-
-
+//@Shadow
+//@Final
+//    public static Block WATER = register(
+//            "water",
+//            new FluidBlock(
+//                    Fluids.WATER,
+//                    AbstractBlock.Settings.create()
+//                            .mapColor(MapColor.WATER_BLUE)
+//                            .replaceable()
+//                            .noCollision()
+//                            .strength(100.0F)
+//                            .pistonBehavior(PistonBehavior.DESTROY)
+//                            .dropsNothing()
+//                            .liquid()
+//                            .sounds(BlockSoundGroup.INTENTIONALLY_EMPTY)
+//            )
+//    );
+//
 
 
 
